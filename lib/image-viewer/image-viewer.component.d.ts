a0623d16e7a72bacc49c6c1051ada93d7f7fa259
huangziyi01 <huangziyi01@baidu.com>
2016-09-14 11:36:17 +0800

    发布组件

--- lib/image-viewer/image-viewer.component.d.ts
+++ lib/image-viewer/image-viewer.component.d.ts
@@ -3,5 +3,21 @@ import * as typings from './image-viewer.type';
 export default class ImageViewer extends React.Component<typings.PropsDefine, typings.StateDefine> {
     static defaultProps: typings.PropsDefine;
     state: typings.StateDefine;
+    private fadeAnim;
+    private standardPositionX;
+    private positionXNumber;
+    private positionX;
+    componentWillMount(): void;
+    loadImage(index: number): void;
+    handleHorizontalOuterRangeOffset(offsetX: number): void;
+    handleResponderRelease(vx: number): void;
+    goBack(): void;
+    goNext(): void;
+    resetPosition(): void;
+    handleLongPress(image: typings.ImageInfo): void;
+    getContent(): JSX.Element;
+    saveToLocal(): void;
+    getMenu(): JSX.Element;
+    handleLeaveMenu(): void;
     render(): JSX.Element;
 }
--- lib/image-viewer/image-viewer.type.d.ts
+++ lib/image-viewer/image-viewer.type.d.ts
@@ -1,5 +1,19 @@
+import * as React from 'react';
 import * as ReactNative from 'react-native';
 export interface PropsDefine extends ReactNative.ViewProperties {
+    imageUrls: Array<ImageInfo>;
+    enableImageZoom?: boolean;
+    onShowModal?: (content?: JSX.Element) => void;
+    visible?: boolean;
+    onCancle?: () => void;
+    flipThreshold?: number;
+    maxOverflow?: number;
+    index?: number;
+    failImageSource?: string | {
+        uri: string;
+    };
+    loadingRender?: () => React.ReactElement<any>;
+    others?: any;
 }
 export declare class PropsGaea {
     gaeaName: string;
@@ -7,8 +21,42 @@ export declare class PropsGaea {
     gaeaUniqueKey: string;
 }
 export declare class Props extends PropsGaea implements PropsDefine {
+    show: boolean;
+    imageUrls: ImageInfo[];
+    enableImageZoom: boolean;
+    onShowModal: () => void;
+    visible: boolean;
+    onCancle: () => void;
+    flipThreshold: number;
+    maxOverflow: number;
+    failImageSource: string;
+    index: number;
+    loadingRender: () => any;
 }
 export interface StateDefine {
+    show?: boolean;
+    currentShowIndex?: number;
+    imageLoaded?: boolean;
+    imageSizes?: Array<ImageSize>;
+    isShowMenu?: boolean;
 }
 export declare class State implements StateDefine {
+    show: boolean;
+    imageLoaded: boolean;
+    currentShowIndex: number;
+    imageSizes: any;
+    isShowMenu: boolean;
+}
+export interface ImageInfo {
+    url: string;
+    width?: number;
+    height?: number;
+    sizeKb?: number;
+    originSizeKb?: number;
+    originUrl?: string;
+}
+export interface ImageSize {
+    width: number;
+    height: number;
+    status: 'loading' | 'success' | 'fail';
 }
