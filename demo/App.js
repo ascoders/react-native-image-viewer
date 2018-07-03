e325ea642543eea47d187b2089c0a5a0300ab78c
ascoders <576625322@qq.com>
2018-07-03 10:26:55 +0800

    upgrade deps

--- demo/App.js
+++ demo/App.js
@@ -14,6 +14,18 @@ const images = [
       source: require('./img.png')
     },
     freeHeight: true
+  },
+  {
+    // Simplest usage.
+    // url: "https://avatars2.githubusercontent.com/u/7970947?v=3&s=460",
+    // url:
+    // "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1527660246058&di=6f0f1b19cf05a64317cbc5d2b3713d64&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0112a85874bd24a801219c7729e77d.jpg",
+    // You can pass props to <Image />.
+    props: {
+      // headers: ...
+      source: require('./img.png')
+    },
+    freeHeight: true
   }
 ];
 
