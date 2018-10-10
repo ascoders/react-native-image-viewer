f7a68f6ab66b1a34d5ebf68d892219246f36959e
黄子毅 <576625322@qq.com>
2018-10-10 22:13:01 +0800

    Update image-viewer.component.tsx

--- src/image-viewer.component.tsx
+++ src/image-viewer.component.tsx
@@ -379,8 +379,7 @@ export default class ImageViewer extends React.Component<Props, State> {
    */
   public handleClick = () => {
     if (this.props.onClick) {
-      //this.props.onClick(this.handleCancel);
-      this.props.onClick(this.state.currentShowIndex);
+      this.props.onClick(this.handleCancel, this.state.currentShowIndex);
     }
   };
 
